ed229d0b141f0d0608ab953f8f5f95f1d9365b1d
Douglas Urner <dlu@canishe.com>
2016-01-31 00:03:07 -0800

    Fix quotes.

--- server/lib/user-accounts.js
+++ server/lib/user-accounts.js
@@ -21,15 +21,15 @@ function guessWho(u) {
   let who = '';
   if ( typeof u.services.google === 'undefined' ) {
     // Non-Google login.
-    if ( typeof u.profile.name !== "undefined" ) {
-      who = u.profile.name + " ";
+    if ( typeof u.profile.name !== 'undefined' ) {
+      who = u.profile.name + ' ';
     }
-    who += "<" + u.emails[0].address + "> (" + u.roles.toString() + ")";
+    who += '<' + u.emails[0].address + '> (' + u.roles.toString() + ')';
   } else {
     let g = u.services.google;
-    who = g.name + " <" + g.email + "> (" + u.roles.toString() + ")";
+    who = g.name + ' <' + g.email + '> (' + u.roles.toString() + ')';
   }
-  who += " " + u.status.lastLogin.ipAddr;
+  who += ' ' + u.status.lastLogin.ipAddr;
   return who;
 }
 
@@ -38,26 +38,26 @@ Accounts.validateNewUser( function (u) {
   // Validate source of Google accounts. We only want accounts associated with
   // our organization.
 
-  logger( "info", "new user:", u );
+  logger( 'info', 'new user:', u );
 
-  if ( typeof u.services.google !== "undefined" ) {
+  if ( typeof u.services.google !== 'undefined' ) {
 
     let g = u.services.google;
 
-    if ( g.email.match("@g.highlineschools.org$") ) {
+    if ( g.email.match('@g.highlineschools.org$') ) {
       return true;
     } else {
-      logger( "warning", "validate: fail:", g.email );
-      throw new Meteor.Error( 403, "Please user your school Google account." );
+      logger( 'warning', 'validate: fail:', g.email );
+      throw new Meteor.Error( 403, 'Please user your school Google account.' );
     }
   } else {
     if ( Meteor.users.find().count() === 0 ) {
       // Bootstrapping - first account is the administrator.
-      logger( "alert", "creating first account: ", u.emails[0].address );
+      logger( 'alert', 'creating first account: ', u.emails[0].address );
       return true;
     } else {
-      logger( "warning", "validate: fail:", u.emails[0].address );
-      throw new Meteor.Error( 403, "Please user your school Google account." );
+      logger( 'warning', 'validate: fail:', u.emails[0].address );
+      throw new Meteor.Error( 403, 'Please user your school Google account.' );
     }
   }
 });
@@ -68,20 +68,20 @@ Accounts.onCreateUser( function (options, u) {
 
   if ( Meteor.users.find().count() === 0 ) {
     // Bootstrapping, create first user account as administrator (admin role).
-    u.roles = [ "admin" ];
-    options.profile.name = "Root";
-    logger("info", "created:", u.emails[0].address, u.roles.toString() );
+    u.roles = [ 'admin' ];
+    options.profile.name = 'Root';
+    logger('info', 'created:', u.emails[0].address, u.roles.toString() );
   } else {
     let g = u.services.google;
-    if ( g.email.match("^[1-9][0-9]+@") ) {
-      u.roles = [ "student" ];
+    if ( g.email.match('^[1-9][0-9]+@') ) {
+      u.roles = [ 'student' ];
     } else {
-      u.roles = [ "teacher" ];
+      u.roles = [ 'teacher' ];
     }
-    logger("info", "created:", g.name, g.email, u.roles.toString() );
+    logger('info', 'created:', g.name, g.email, u.roles.toString() );
   }
 
-  if ( typeof options.profile !== "undefined" ) {
+  if ( typeof options.profile !== 'undefined' ) {
     u.profile = options.profile;
   } else {
     u.profile = {};
